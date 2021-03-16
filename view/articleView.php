d6df4a1e45f75a112ca30275a92d8e8c7f905086
dalexisd <dalexis.aformac2021@gmail.com>
2021-03-16 08:24:09 +0000

    test6

---
+++ view/articleView.php
@@ -0,0 +1,22 @@
+<?php include 'partials/header.php';?>
+
+
+<div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+        <span><?php echo $users['lastname'] . " " . $users'firstname']?></span>
+        </div>
+        <div class="card-action">
+          <a href="#"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+
+<?php include 'partials/footer.php';?>
---
+++ view/index.php
@@ -0,0 +1,21 @@
+<?php
+include 'header.php';
+
+?>
+<body>
+    <div class= "container">
+      <h1>BLOG SPORT</h1>
+
+      <?php
+
+
+
+?>
+
+<?php
+
+ include 'footer.php';
+ ?>       
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</html>
