330693ab57f4e68c682aa2ebe374876276d0eb5c
trds-dev <trds@live.fr>
2021-03-16 14:25:58 +0100

    'maj_formulaire'

--- formulaire.php
+++ formulaire.php
@@ -1,59 +1,53 @@
 <?php
-include 'header.php'
+
+include 'view/partials/header.php';
+
 
 ?>
 
+<h1>Inscription</h1>
         <div class=row>
-            <div class="col md-12 md-6">
+            <div class="col-3 text-center">
 
-            <form method="post" action="traitement.php">
+                <form method="post" action="traitement.php">
 
-            <h4>lastname</h4>
-            <input type="text" id="lastname" required>
+                <h5>lastname</h5>
+                <input type="text" id="lastname" required>
 
-            <h4>firstname</h4>
-            <input type="text" id="firstname" required>
+                <h5>firstname</h5>
+                <input type="text" id="firstname" required>
 
-            <h4>pseudo</h4>
-            <input type="text" id="pseudo" required>
+                <h5>pseudo</h5>
+                <input type="text" id="pseudo" required>
+            </div> 
+                <div class="col-9">
+                <h5>email</h5>
+                <input type="mail" id="mail" required>
 
-            <h4>email</h4>
-            <input type="mail" id="mail" required>
-
-            <h4>password</h4>
-            <input type="password" id="password" required>
-           
-            <h4>date-inscrition</h4>
-            <?php
-                $date = date("d-m-Y");
-                $heure = date("H:i");
-                Print(" Le $date à $heure");
-            ?>
-
+                <h5>password</h5>
+                <input type="password" id="password" required>
             
+                <h5>date-inscrition :</h5>
+                <?php
+                    $date = date("d-m-Y");
+                    $heure = date("H:i");
+                    Print("$date à $heure");
+                ?>
+            <h5>Gender</h5>
 
-        <div class=form2>
-        <div class=row>
-            <div class="col -12 md-6">
+                <input type="radio" id="genderH"
+                name="homme" value="homme">
+                <label for="contactChoice1">H</label>
 
-        <input type="radio" id="genderH"
-            name="homme" value="homme">
-            <label for="contactChoice1">H</label>
+                <input type="radio" id="genderF"
+                name="femme" value="femme">
+                <label for="contactChoice2">F</label>
 
-            <input type="radio" id="genderF"
-            name="femme" value="femme">
-            <label for="contactChoice2">F</label>
+                <button type="submit">Envoyer</button>
 
-
-            <button type="button">Envoyer</button>
-
-        
             </div>
         </div>
 
-</div>
-</form>
-<?php
-include 'footer.php'
 
-?>
+</form>
+
--- view/partials/header.php
+++ view/partials/header.php
@@ -3,7 +3,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/80bb5420f9.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles.css">
